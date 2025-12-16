4d469dda6901eeab2efabcdccbf5777fbc398001
Adan <adan.butt@ec.gc.ca>
2025-12-16 20:23:17 +0000

    compressiom level 4

--- pygeoapi/api/__init__.py
+++ pygeoapi/api/__init__.py
@@ -153,7 +153,7 @@ def apply_gzip(headers: dict, content: Union[str, bytes]) -> Union[str, bytes]:
             else:
                 headers['Content-Type'] = \
                     f"{headers['Content-Type']}; charset={charset}"
-                content = compress(content.encode(charset), compresslevel=1)
+                content = compress(content.encode(charset), compresslevel=4)
         except TypeError as err:
             headers.pop('Content-Encoding', None)
             LOGGER.error(f'Error in compression: {err}')
